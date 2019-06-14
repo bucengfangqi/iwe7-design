ee9649d7894875670a76e7d1dabaa5dd7d43e3b6
bear_c <1639501830@qq.com>
2019-06-14 11:06:48 +0800

    完成标题

--- src/lib/components/iwe6-design-dashboard/public_api.ts
+++ src/lib/components/iwe6-design-dashboard/public_api.ts
@@ -1,18 +1,23 @@
-import { BoardComponent } from "./board/board.component";
-import { BoardSettingComponent } from "./board-setting/board-setting.component";
+import { BoardComponent } from './board/board.component';
+import { BoardSettingComponent } from './board-setting/board-setting.component';
 
 export const component = {
-  title: "图表",
-  name: "board",
+  title: '图表',
+  name: 'board',
   show: true,
   setting: BoardSettingComponent,
   preview: BoardComponent,
   props: {
-    name: "board",
-    title: "请输入大标题",
+    name: 'board',
+    title: '标题文本',
+    titleColor: '#FFF',
+    titleBg: '#7f6235',
+    titlePosition: 'center',
+    titleFontSize: 22,
+    titlelineHeight: 45,
     style: {
       display: 'block',
-      [`background-color`]: '#efefef'
+      [`background-color`]: '#efefef',
     },
     props: []
   }
